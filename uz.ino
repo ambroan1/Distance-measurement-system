7aa6bd4dbbf904d0a9a6d5590057256c00d83138
Kůtová, Magdaléna <kutovmag@fel.cvut.cz>
2023-11-13 19:45:47 +0000

    Upload New File

---
+++ uz.ino
@@ -0,0 +1,55 @@
+
+const int TRIG_PIN = 6;
+const int ECHO_PIN = 7;
+const int BUZZER_PIN = 3;
+const int DISTANCE_THRESHOLD = 50;
+const int MOTOR_PIN = 10;
+
+float duration_us, distance_cm;
+
+int gradual(float dist)
+{
+  if (dist>40) return 500;
+  if (dist>30) return 1000;
+  if (dist>20) return 1500;
+  if (dist>10) return 2000;
+  if (dist<=10) return 3000;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(MOTOR_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+    // measure duration of pulse from ECHO pin
+  duration_us = pulseIn(ECHO_PIN, HIGH);
+  // calculate the distance
+  distance_cm = 0.017 * duration_us;
+
+  if(distance_cm < DISTANCE_THRESHOLD) {
+    //digitalWrite(BUZZER_PIN, HIGH); // turn on Piezo Buzzer
+
+    tone(BUZZER_PIN, gradual(distance_cm));
+    digitalWrite(MOTOR_PIN,LOW);
+  }
+  else{
+    //digitalWrite(BUZZER_PIN, LOW);  // turn off Piezo Buzzer
+    noTone(BUZZER_PIN);
+    digitalWrite(MOTOR_PIN,HIGH);
+  }
+ // print the value to Serial Monitor
+  Serial.print("distance: ");
+  Serial.print(distance_cm);
+  Serial.println(" cm");
+  //delay(500);
+}
